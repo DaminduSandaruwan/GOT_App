2261cd207662e7a889f7d183368808194b85f0a9
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-05-16 22:29:47 +0530

    Add Characters

--- lib/models/characters.dart
+++ lib/models/characters.dart
@@ -25,4 +25,20 @@ List characters = [
     "Queen Daenerys I Targaryen is the younger sister of Rhaegar Targaryen and Viserys Targaryen, the paternal aunt of Jon Snow, and the youngest child of King Aerys II Targaryen and Queen Rhaella Targaryen, who were both ousted from the Iron Throne during Robert Baratheon's rebellion.",
       colors: [Colors.grey.shade800, Colors.grey.shade800]
   ),
+
+  Character(
+    name: "Arya Stark",
+    imagePath: "assets/images/AryaStark.png",
+    description:
+    "Arya Stark is the third child of Eddard Stark and Catelyn Stark. After narrowly escaping the persecution of House Stark by House Lannister, Arya is trained as a Faceless Man at the House of Black and White in Braavos, and uses her new skills to bring those who have wronged her family to justice.",
+      colors: [Colors.grey.shade800, Colors.grey.shade800]
+  ),
+
+  Character(
+    name: "Sansa Stark",
+    imagePath: "assets/images/QueenSansa.png",
+    description:
+    "Sansa Stark is the eldest daughter of Eddard Stark of Winterfell and his wife Catelyn. She initially starts off with a very naive view of the world, but as time goes on and she and her family suffer one cruelty and betrayal after another, she becomes a more hardened and learned individual.",
+      colors: [Colors.grey.shade800, Colors.grey.shade800]
+  ),
 ];
